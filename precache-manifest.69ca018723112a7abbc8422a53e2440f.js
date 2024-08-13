49d179d4791b9174359b3717b577cb796bbc06c9
Irina1781 <press.zim@yandex.ru>
2024-08-13 22:20:00 +0300

    Add files via upload

---
+++ precache-manifest.69ca018723112a7abbc8422a53e2440f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a474e19234a714e6144bc230d9c46182",
+    "url": "/index.html"
+  },
+  {
+    "revision": "770466abe61f1e39a837",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "49d53756381de06665bb",
+    "url": "/static/js/2.ba7e1342.chunk.js"
+  },
+  {
+    "revision": "770466abe61f1e39a837",
+    "url": "/static/js/main.b5bd3de0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
